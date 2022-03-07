3be5cf0eccdeb87f850c54ed211c79e2c3b9c518
sayan das <sayanwastaken@gmail.com>
2022-03-07 23:43:19 +0530

    some changes

--- index.js
+++ index.js
@@ -1,56 +1,54 @@
-const getData=async()=>{
+const getData = async () => {
+  let clientId = "141ad48a8c03765";
+  let NewHeader = new Headers();
+  NewHeader.append("Authorization", `Client-ID ${clientId}`);
+  let methodObj = {
+    method: "GET",
+    headers: NewHeader,
+    redirect: "follow",
+  };
+  //https://api.imgur.com/3/gallery/top/viral/day/1?showViral=true&mature=false&album_previews=true
 
-    let clientId ="141ad48a8c03765"
-    let NewHeader = new Headers();
-    NewHeader.append("Authorization", `Client-ID ${clientId}`);
-    let methodObj = {
-        method: 'GET',
-        headers: NewHeader,
-        redirect: 'follow'
-    };
-    //https://api.imgur.com/3/gallery/top/viral/day/1?showViral=true&mature=false&album_previews=true
+  let data = await fetch(
+    "https://api.imgur.com/3/gallery/top/viral/day/1?showViral=true&mature=false&album_previews=true",
+    methodObj
+  );
+  let res = await data.json();
+  // console.log(res.data[0].images[0].gifv)
 
-    let  data= await fetch("https://api.imgur.com/3/gallery/top/viral/day/1?showViral=true&mature=false&album_previews=true",methodObj)
-    let res=await data.json()
-    // console.log(res.data[0].images[0].gifv)
-    
-    showData(res.data)
-    let imgsArr=res.data
-    
-}
-getData()
+  showData(res.data);
+  let imgsArr = res.data;
+};
+getData();
 
+const showData = (data) => {
+  data.map((elem) => {
+    if (elem.images) {
+      if (elem.images.length >= 0) {
+        let arr = elem.images;
 
-const showData=(data)=>{
-   data.map((elem)=>{
-       if(elem.images){
-           if(elem.images.length>=0){
-               let arr=elem.images
-               
-               for(let i=0;i<arr.length;i++){
-                   if(arr[i].type==="image/png"||arr[i].type==="image/jpeg"){
-                    let cardCont= document.getElementById("cardsDivCont")
-                    let singleCard=document.createElement("div");
-                    singleCard.setAttribute=("class", "singleCard");
-                    let cardImg=document.createElement("div");
-                    cardImg.setAttribute=("class", "cardImg");
-                    let img=document.createElement("img");
-                    img.src=arr[i].link;
-                    cardImg.appendChild(img);
-                    let cardLikes=document.createElement("div");
-                    cardLikes.setAttribute=("class", "cardLikes");
-                    let likesTitle=document.createElement("div")
-                    likesTitle.setAttribute=("class", "likesTitle");
-                    let likesLogos=document.createElement("div");
-                    likesLogos.setAttribute=("class", "likesLogos");
-                    cardLikes.append(likesTitle,likesLogos);
-                    singleCard.append(cardImg,cardLikes);
-
-                    cardCont.append(singleCard);
-                    
-                   }
-               }
-           }
-       }
-   })
-}
+        for (let i = 0; i < arr.length; i++) {
+          if (arr[i].type === "image/png" || arr[i].type === "image/jpeg") {
+            let cardCont = document.getElementById("cardsDivCont");
+            let singleCard = document.createElement("div");
+            singleCard.setAttribute("class", "singleCard");
+            let cardImg = document.createElement("div");
+            cardImg.setAttribute("class", "cardImg");
+            let img = document.createElement("img");
+            img.src = arr[i].link;
+            cardImg.appendChild(img);
+            let cardLikes = document.createElement("div");
+            cardLikes.setAttribute("class", "cardLikes");
+            let likesTitle = document.createElement("div");
+            likesTitle.setAttribute("class", "likesTitle");
+            let likesLogos = document.createElement("div");
+            likesLogos.setAttribute("class", "likesLogos");
+            cardLikes.append(likesTitle, likesLogos);
+            singleCard.append(cardImg, cardLikes);
+            cardCont.append(singleCard);
+          }
+        }
+      }
+    }
+  });
+};
